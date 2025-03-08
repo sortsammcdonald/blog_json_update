91696eda985805e5f07dd1b9d273aa8d5161b1a6
Sam McDonald <40240233+sortsammcdonald@users.noreply.github.com>
2025-03-08 16:52:53 +0100

    Add files via upload

--- json_processing.py
+++ json_processing.py
@@ -1,24 +1,35 @@
 import json
 
-# Load JSON data from a file
-with open('entries.json', 'r') as file:
-    data = json.load(file)
+class CorrectJson:
+    def __init__(self, filepath):
+        self.filepath = filepath
 
-def remove_all_duplicates(data):
-    seen = set()
-    unique_data = []
-    for item in data:
-        # Convert dictionary to a frozenset of its items to make it hashable
-        item_frozenset = frozenset(item.items())
-        if item_frozenset not in seen:
-            seen.add(item_frozenset)
-            unique_data.append(item)
-    return unique_data
+    def extract_entries(self):
+        # Load JSON data from a file
+        with open(self.filepath, 'r') as file:
+            data = json.load(file)
+
+    def remove_all_duplicates(data):
+        seen = set()
+        unique_data = []
+        for item in data:
+            # Convert dictionary to a frozenset of its items to make it hashable
+            item_frozenset = frozenset(item.items())
+            if item_frozenset not in seen:
+                seen.add(item_frozenset)
+                unique_data.append(item)
+        return unique_data
+    
+class GenJson
+    def __init__(self, json_to_check):
+        self.json_to_check = json_to_check
+
+    def gen_correct_json(self):
+        with open(self.json_to_check, 'w') as file:
+            json.dump(self.json_to_check, file, indent=4)
+   
 
 # Remove duplicates from the data
-cleaned_data = remove_all_duplicates(data)
 
 # Save the updated data back to the file
-with open('entries.json', 'w') as file:
-    json.dump(cleaned_data, file, indent=4)
 
