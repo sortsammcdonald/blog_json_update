9a545f333bc1b7293399f7f0f2423f6840299536
Sam McDonald <40240233+sortsammcdonald@users.noreply.github.com>
2025-03-14 16:54:52 +0100

    Add files via upload

--- json_processing.py
+++ json_processing.py
@@ -1,15 +1,17 @@
 import json
 
 class CorrectJson:
-    def __init__(self, filepath):
+    def __init__(self, filepath, output_filepath):
         self.filepath = filepath
+        self.output_filepath = output_filepath  # Add output file path
 
     def extract_entries(self):
         # Load JSON data from a file
         with open(self.filepath, 'r') as file:
             data = json.load(file)
+        return data  # Return data so it can be used
 
-    def remove_all_duplicates(data):
+    def remove_all_duplicates(self, data):
         seen = set()
         unique_data = []
         for item in data:
@@ -18,16 +20,21 @@ class CorrectJson:
             if item_frozenset not in seen:
                 seen.add(item_frozenset)
                 unique_data.append(item)
-        return unique_data
-    
-class GenJson
-    def __init__(self, json_to_check):
-        self.json_to_check = json_to_check
+        return unique_data  # Return the cleaned list
 
     def gen_correct_json(self):
-        with open(self.json_to_check, 'w') as file:
-            json.dump(self.json_to_check, file, indent=4)
-   
+        # Extract and process the data
+        data = self.extract_entries()
+        unique_data = self.remove_all_duplicates(data)
+
+        # Write cleaned data to output file
+        with open(self.output_filepath, 'w') as file:
+            json.dump(unique_data, file, indent=4)
+
+
+def main():
+    json_to_check = CorrectJson('entries.json','entries.json')
+    json_to_check.gen_correct_json()
 
 # Remove duplicates from the data
 
